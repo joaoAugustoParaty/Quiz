6f29a4d5c049528f11b8f6eeee0377c339d12ca0
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-05-23 19:13:22 -0300

    Dia 2 Question

---
+++ js/questions.js
@@ -0,0 +1,28 @@
+//Creating an array and passing the number, questions, options, and answers
+
+let questions = [
+    {   
+        numb: 1,
+        question: "What dodes HTML stand for?",
+        question: "What does HTML stand for?",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hype Text Multiple Language",
+            "Hyper Tool Multi Language"
+            
+        ]
+    },
+    {   
+        numb: 1,
+        question: "What dodes HTML stand for?",
+        question: "What does HTML stand for?",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hype Text Multiple Language",
+            "Hyper Tool Multi Language"
+            
+        ]
+    },
+];
