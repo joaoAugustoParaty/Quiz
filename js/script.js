03c606dbdcef0edc5e8df6a1d15830290f394b40
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-05-25 16:28:16 -0300

    Dia 4

--- js/script.js
+++ js/script.js
@@ -25,12 +25,24 @@ continue_btn.onclick = ()=>{
 
 let que_count = 0;
 
+const next_btn = quiz_box.querySelector(".next_btn")
+
+//If Next Button Clicked
+next_btn.onclick = ()=>{
+    que_count++
+    showQuestions(que_count)
+    
+}
+
 //getting questions and options form array
 function showQuestions(index){
     const que_text = document.querySelector(".que_text")
     const option_list = document.querySelector(".option_list")
     let que_tag = '<span>'+ questions[index].question +'</span>'
-    let option_tag = '<div class="option"><span></span></div>'
-    + '<div class="option"><span></span></div>'
+    let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
+    + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
+    + '<div class="option">'+ questions[index].options[2] +'<span></span></div>'
+    + '<div class="option">'+ questions[index].options[3] +'<span></span></div>'
     que_text.innerHTML = que_tag
+    option_list.innerHTML = option_tag
 }
