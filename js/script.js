c903ca0f567c33d884f0138cdf8b0a2558aa2a29
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-05-28 18:04:40 -0300

    Add numb questions

--- js/script.js
+++ js/script.js
@@ -20,10 +20,12 @@ continue_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo") //hide the info box
     quiz_box.classList.add("activeQuiz") //show the quiz box
     showQuestions(0)
+    queCounter(1)
 
 }
 
-let que_count = 0;
+let que_count = 0
+let que_numb = 1
 
 const next_btn = quiz_box.querySelector(".next_btn")
 
@@ -31,7 +33,9 @@ const next_btn = quiz_box.querySelector(".next_btn")
 next_btn.onclick = () =>{
     if(que_count < questions.length - 1){
         que_count++
+        que_numb++
         showQuestions(que_count)
+        queCounter(que_numb)
     }else{
         console.log("Questions completed")
     }
@@ -49,4 +53,11 @@ function showQuestions(index){
     + '<div class="option">'+ questions[index].options[3] +'<span></span></div>';
     que_text.innerHTML = que_tag
     option_list.innerHTML = option_tag
+}
+
+// Contagem de perguntas
+function queCounter(index){
+const bottom_ques_counter = quiz_box.querySelector(".total_que")
+let totalQuestCountTag = '<span><p>'+ index +'</p>of<p>'+ questions.length +'</p>Questions</span>'
+bottom_ques_counter.innerHTML = totalQuestCountTag
 }
