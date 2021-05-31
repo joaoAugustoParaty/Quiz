7ae657afff732445a3f1f7eb4759d3a2799fc4a3
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-05-31 16:00:48 -0300

    Dia 5

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const exit_btn = info_box.querySelector(".buttons .quit")
 const continue_btn = info_box.querySelector(".buttons .restart")
 const quiz_box = document.querySelector(".quiz_box")
 
+const option_list = document.querySelector(".option_list")
+
 //If Start Quiz Button Clicked
 start_btn.onclick = ()=>{
     info_box.classList.add("activeInfo") //show the info boz
@@ -45,7 +47,6 @@ next_btn.onclick = () =>{
 //getting questions and options form array
 function showQuestions(index){
     const que_text = document.querySelector(".que_text")
-    const option_list = document.querySelector(".option_list")
     let que_tag = '<span>'+ questions[index].numb +". "+ questions[index].question +'</span>'
     let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
     + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
@@ -59,19 +60,37 @@ function showQuestions(index){
     }
 }
 
+let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>'
+let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>'
+
 function optionSelected(answer){
     let userAns = answer.textContent
     let correctAns = questions[que_count].answer
+    let allOptions = option_list.children.length 
+
     if(userAns == correctAns){
         answer.classList.add("correct")
         console.log("Answer is Correct")
+        answer.insertAdjacentHTML("beforeend", tickIcon)
     }else{
         answer.classList.add("incorrect")
         console.log("Answer is Wrong")
+        answer.insertAdjacentHTML("beforeend", crossIcon)
+
+        //if answers is incorrect then automtically selected the correct answer
+        for (let i = 0; i < allOptions; i++) {
+            if(option_list.children[i].textContent == correctAns){
+                option_list.children[i].setAttribute("class", "option correct")
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIcon)
+            }
+        }
     }
 
-    //Once user selected
-    
+    //Once user selected disabled all options
+    for(let i = 0; i < allOptions; i++) {
+        option_list.children[i].classList.add("disabled")
+    }
+
     
 }
 // Contagem de perguntas
